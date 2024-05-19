a38816301e1e40e2e8bc331383ede93972fcde4f
Krystyna Banaszewska <krystyna.banaszewska.ext@nokia.com>
2024-05-19 22:40:56 +0200

    labels fixed, small adjustments

--- VGG/VGG19/main_experiments.py
+++ VGG/VGG19/main_experiments.py
@@ -2,5 +2,5 @@ from ..implementation import main_experiment
 
 # Repeat the experiment 4 times to maintain reliable results
 
-for i in range(1, 5):
-    main_experiment(i, 'VGG19')
+for x in range(1, 5):
+    main_experiment(i=x, vgg_model_type='VGG19', epochs=10, size=50000)
